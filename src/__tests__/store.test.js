6e537770f8d694614ebdf3fab6b9074b4358c5e5
Antwi Eric <ANTWI@DESKTOP-92MFH60.(none)>
2023-05-28 00:48:44 +0000

    fix linter errors

--- src/__tests__/store.test.js
+++ src/__tests__/store.test.js
@@ -13,37 +13,66 @@ describe('Home', () => {
     const store = mockStore({
       pollutants: {
         pollutants: {
-          coord: {lon: 1, lat: 7,},
+          coord: { lon: 1, lat: 7 },
           list: [
             {
-              main: {aqi: 1},
-              components: { co:370.5, no:0, no2:0.42, o3:26.46, so2:0.24, pm2_5:5.44, pm10:6.06, nh3:0.33,},
+              main: { aqi: 1 },
+              components: {
+                co: 370.5,
+                no: 0,
+                no2: 0.42,
+                o3: 26.46,
+                so2: 0.24,
+                pm2_5: 5.44,
+                pm10: 6.06,
+                nh3: 0.33,
+              },
               dt: 1685206800,
             },
             {
-              main: {aqi: 1},
-              components: { co:367.5, no:0.56, no2:0.98, o3:36.46, so2:30.24, pm2_5:8.44, pm10:60.06, nh3:0.13,},
+              main: { aqi: 1 },
+              components: {
+                co: 367.5,
+                no: 0.56,
+                no2: 0.98,
+                o3: 36.46,
+                so2: 30.24,
+                pm2_5: 8.44,
+                pm10: 60.06,
+                nh3: 0.13,
+              },
               dt: 1685210400,
             },
-          ]
+          ],
         },
         isloading: false,
         error: false,
       },
       filter: {
         filter: [
-            {
-                main: {aqi: 1},
-                components: { co:370.5, no:0, no2:0.42, o3:26.46, so2:0.24, pm2_5:5.44, pm10:6.06, nh3:0.33,},
-                dt: 1685206800,
-              },
-              {
-                main: {aqi: 1},
-                components: { co:367.5, no:0.56, no2:0.98, o3:36.46, so2:30.24, pm2_5:8.44, pm10:60.06, nh3:0.13,},
-                dt: 1685210400,
-              },
-        ]
-      }
+          {
+            main: { aqi: 1 },
+            components: {
+              co: 370.5, no: 0, no2: 0.42, o3: 26.46, so2: 0.24, pm2_5: 5.44, pm10: 6.06, nh3: 0.33,
+            },
+            dt: 1685206800,
+          },
+          {
+            main: { aqi: 1 },
+            components: {
+              co: 367.5,
+              no: 0.56,
+              no2: 0.98,
+              o3: 36.46,
+              so2: 30.24,
+              pm2_5: 8.44,
+              pm10: 60.06,
+              nh3: 0.13,
+            },
+            dt: 1685210400,
+          },
+        ],
+      },
     });
 
     render(
